07df881471e4297c1276e6be261c975da896441f
tawfiq58 <tawfiqhasan58@gmail.com>
2022-10-03 12:52:36 +0600

    Add aws auth

---
+++ AMPLIFY.md
@@ -0,0 +1,20 @@
+#
+
+Init with github
+check CI/CD deployment is working
+
+sudo npm install -g @aws-amplify/cli
+amplify configure
+
+Step by step config AWS
+ - IAM creation
+ - accessKey pass
+
+
+backend-environment setup
+amplify pull --appId d23szvkyqyahwq --envName staging
+Give access and login with aws-amplify
+
+npm install aws-amplify @aws-amplify/ui-react
+amplify add auth
+amplify push --y
--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Edited and CI/CD deployment automatically with AWS amplify</h1>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
